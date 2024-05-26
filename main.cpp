5861d859082bf67ed4f0c799911ef4a04f39fca2
Serhii Ponomarenko <serhii.ponomarenko@aerq.com>
2024-05-26 20:30:51 +0200

    tests: added some tests

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <numeric>
+
+#include <cxxabi.h>
+
+// Implementation
 
 template <typename Operation, typename ElementType>
 class SegmentTree
@@ -76,13 +82,76 @@ private:
     }
 };
 
+
+
+// **** TESTS ******
+
+std::string demangle(const char* name)
+{
+    char buffer[1024];
+    size_t size=1024;
+    int status;
+    return abi::__cxa_demangle(name, buffer, &size, &status);
+}
+
+template <typename Operation, typename ElementType>
+class TestSegmentTree
+{
+public:
+    TestSegmentTree(const std::vector<ElementType>& arr) : data(arr), tree(data){} 
+
+public:
+    std::vector<ElementType> data;
+    SegmentTree<Operation, ElementType> tree;
+
+public:
+    void check_query(int from, int to)
+    {
+        auto result = tree.query(from, to);
+        auto expected = std::accumulate(data.begin() + from + 1, data.begin() + to + 1, data[from], Operation());
+        if (result != expected)
+            std::cout << "FAILURE: [" << demangle(typeid(*this).name()) << "] Query(" << from << "," << to << "): " << result << " (" << expected << " is expected)" << std::endl;
+    }
+
+};
+
 int main()
 {
-    std::vector<int> numbers = {1, 3, 5};
-    SegmentTree<std::plus<>, int> segTree(numbers);
+    TestSegmentTree<std::multiplies<>, int> test1({1,3,5});
+    test1.check_query(0, 2);
+    test1.check_query(0, 0);
+    test1.check_query(1, 1);
+    test1.check_query(2, 2);
+    test1.check_query(1, 2);
+    test1.check_query(0, 1);
 
-    std::cout << "Sum(0,2): " << segTree.query(0, 2) << std::endl;
-    std::cout << "Update(1,2)" << std::endl;
-    segTree.update(1, 2);
-    std::cout << "Sum(0,2): " << segTree.query(0, 2) << std::endl;
+    TestSegmentTree<std::plus<>, int> test2({1,3,5});
+    test2.check_query(0, 2);
+    test2.check_query(0, 0);
+    test2.check_query(1, 1);
+    test2.check_query(2, 2);
+    test2.check_query(1, 2);
+    test2.check_query(0, 1);
+
+    // Test plus carefully, with different sizes from 1 to 100
+    for (int i = 1; i < 100; ++i)
+    {
+        std::vector<int> data;
+        for (int x = 1; x <= i; ++x)
+        {
+            data.push_back(x);
+        }
+        TestSegmentTree<std::plus<>, int> test(data);
+        for (int l = 0; l < data.size(); ++l)
+            for (int r = l; r < data.size(); ++r)
+                test.check_query(l, r);
+
+        for (int change = 0; change < data.size(); ++change)
+        {
+            test.tree.update(change, change + 100);
+            for (int l = 0; l < data.size(); ++l)
+                for (int r = l; r < data.size(); ++r)
+                    test.check_query(l, r);
+        }
+    }
 }
